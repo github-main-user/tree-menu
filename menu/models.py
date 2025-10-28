bc3c8a0d9e94467a36994c4990d302eb00e391be
github-main-user <kolya.skripn@mail.ru>
2025-10-28 16:21:25 +0300

    feat: add menu specific models

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,46 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.urls import NoReverseMatch, reverse
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.SlugField(unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="items")
+    title = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE, related_name="children"
+    )
+
+    url = models.CharField(max_length=255)
+    named_url = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        if self.named_url:
+            try:
+                return reverse(self.url)
+            except NoReverseMatch:
+                return "#"
+        return self.url
+
+    def clean(self):
+        if self.parent:
+            if self.parent == self:
+                raise ValidationError("Element can't be parent of him self")
+
+            ancestor = self.parent
+            while ancestor:
+                if ancestor == self:
+                    raise ValidationError("Circular dependency in the menu")
+                ancestor = ancestor.parent
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["id"]
