0a9e930e0bac41ae55dc35d20c69611477e90a09
github-main-user <kolya.skripn@mail.ru>
2025-10-29 16:21:25 +0300

    feat: setup admin panel for menu and menu item models

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, MenuItem
+
+
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    extra = 1
+    fk_name = "menu"
+    fields = ("title", "parent", "url")
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    inlines = [MenuItemInline]
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("menu", "title", "parent", "url", "named_url")
+    search_fields = ("title",)
+    list_filter = ("menu__name", "parent", "named_url")
